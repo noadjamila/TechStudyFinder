b5f5027fc4cdca00c1830fdc3914588515091a05
noadjamila <noa@initree.de>
2025-10-29 18:38:49 +0100

    Update server/index.js
    compiled React files bereitstellen
    
    Co-authored-by: Paula <166331151+psophis@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 // Test-Route
 app.get("/api/hello", (_req, res) => {
