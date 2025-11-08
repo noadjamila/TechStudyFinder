7633fe47dbac9acf0a76005329a6b11e23eb3c72
Reem <Reemalsheikha5@gmail.com>
2025-11-08 02:45:13 +0100

    Implement design concept for GI-119

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import './App.css';
 
 function App() {
   const [msg, setMsg] = useState("");
@@ -17,6 +18,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <img src="/logo.png" className="App-logo" alt="logo" />
+          <h1>Tech Study Finder</h1>
+          <p>Finde den Studiengang, der zu dir passt!</p>
+          <p>{msg}</p>
+          <button className="btn-primary">Quiz starten</button>
+          <a href="https://reactjs.org/" className="btn-link" target="_blank" rel="noopener noreferrer">Erkunde React</a>
         <div>
           <h1>Backend test message:</h1>
           <p>{msg}</p>
