d80f33e3213f27ef06dd3dc28e3e902b6e6e58f7
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-13 13:44:12 +0100

    Set up function to run the shell script

---
+++ server/src/services/deployment.utils.ts
@@ -0,0 +1,23 @@
+import { exec } from "child_process";
+
+/**
+ * Executes the deployment shell script.
+ * @returns Promise that resolves when the script execution is complete.
+ */
+export function runDeploymentScript(): Promise<void> {
+  const SCRIPT_PATH = "./deploy.sh";
+
+  return new Promise((resolve, reject) => {
+    exec(`sh ${SCRIPT_PATH}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`[Deployment Script] Error: ${error.message}`);
+        return reject(error);
+      }
+      if (stderr) {
+        console.error(`[Deployment Script] Stderr: ${stderr}`);
+      }
+      console.log(`[Deployment Script] Deplyoment successful: ${stdout}`);
+      resolve();
+    });
+  });
+}
