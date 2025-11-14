68f9d038a6f8c35e03be6f1e32b96c2fd737854e
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-14 20:49:19 +0100

    Fix type

--- server/src/routes/deploy.route.ts
+++ server/src/routes/deploy.route.ts
@@ -3,6 +3,6 @@ import { handleWebhook } from "../controllers/deploy.controller";
 
 const deployRouter = Router();
 
-deployRouter.post("/webhook", handleWebhook);
+deployRouter.post("/webhook", handleWebhook as unknown as RequestHandler);
 
 export default deployRouter;
