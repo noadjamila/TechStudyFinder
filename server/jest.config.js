2fa7a45bb401cc36569586bf041273b39d08a023
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-14 18:36:19 +0100

    Adjust config after moving tests back into service directory

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,10 +1,6 @@
 /** @type {import('jest').Config} */
 module.exports = {
   rootDir: '.',
-  roots: [
-    "<rootDir>/src",
-    "<rootDir>/__tests__"
-  ],
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
