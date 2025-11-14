0627769b7740bde84becc47d3ffb30f1341c6dbd
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-14 21:03:20 +0100

    Remove '__tests__' folder

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   rootDir: ".",
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  testMatch: ["**/?(*.)+(spec|test).ts"],
   transformIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
 };
