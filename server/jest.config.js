bdcf23f3af6168e8458b75b7d53ea7926ba6bed9
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-16 11:30:26 +0100

    Resolve merge conflicts

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,16 +1,9 @@
 /** @type {import('jest').Config} */
 module.exports = {
-<<<<<<< HEAD
-  rootDir: '.',
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
-=======
   rootDir: ".",
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/?(*.)+(spec|test).ts"],
->>>>>>> c8095d9 (GI-124: Set up GH actions workflow for CI (#15))
   transformIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
 };
