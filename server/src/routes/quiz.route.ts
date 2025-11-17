23e3fb9580cd1eecde6d545fb25ab6529ac4efd1
DamarisLi <158483287+DamarisLi@users.noreply.github.com>
2025-11-17 15:05:59 +0100

    GI-88: Add filtering logic for level 1 (#8)
    * Implement request and response between frontend and backend
    
    * adds (non functional) test button for sending data to backend
    
    * Level Filter erst im Service splitten + Dateinamen ändern
    
    * implement filtering logic for level 1
    
    * fix eslint issues
    
    * Add backend tests
    
    * edit test config file, fix linting issues
    
    * clarify quiz route

---
+++ server/src/routes/quiz.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { filterLevel } from "../controllers/quiz.controller";
+
+const router = express.Router();
+
+router.post("/filter", filterLevel);
+
+export default router;
