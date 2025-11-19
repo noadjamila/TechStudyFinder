48ee2b6c50d24d2a92327bde989bd6190a487dd0
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-19 20:19:57 +0100

    Disable no console warning for service worker (forn now)

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,9 +1,11 @@
 self.addEventListener("install", () => {
+  // eslint-disable-next-line no-console
   console.log("[Service Worker] Installed");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", () => {
+  // eslint-disable-next-line no-console
   console.log("[Service Worker] Activated");
 });
 
