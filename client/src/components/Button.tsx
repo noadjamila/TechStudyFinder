67766bd8ff458a0a5b01714b99dd575fbb70650b
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-19 14:50:41 +0100

    Reference theme palette instead of using hard-coded color strings

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -4,15 +4,15 @@ export default function Button() {
   return (
     <MUIButton
       variant="contained"
-      sx={{
-        backgroundColor: "lightGrey",
-        color: "darkGrey",
+      sx={(theme) => ({
+        backgroundColor: theme.palette.grey[100],
+        color: theme.palette.grey[900],
         borderRadius: "20px",
         boxShadow: 3,
         ":hover": {
-          backgroundColor: "purple",
+          backgroundColor: theme.palette.primary.main,
         },
-      }}
+      })}
     >
       Button
     </MUIButton>
