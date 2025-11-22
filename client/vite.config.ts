4e73cd6aa527cb7bae66fb473756db268004ba24
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-22 11:43:32 +0100

    Add missing Vite setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
+    },
   },
 });
