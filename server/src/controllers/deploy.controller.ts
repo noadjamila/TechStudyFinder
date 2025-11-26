d8b161ae8fd13c63aa4913bf3b9ab9e9de8649d0
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-11-26 20:41:28 +0100

    Decouple the promise

--- server/src/controllers/deploy.controller.ts
+++ server/src/controllers/deploy.controller.ts
@@ -53,10 +53,12 @@ export const handleWebhook = async (
       return res.status(200).json({ message: "No deployment needed" });
     }
 
-    runDeploymentScript()
-      // eslint-disable-next-line no-console
-      .then(() => console.log("Deployment finished"))
-      .catch((e) => console.error("Deployment error:", e));
+    setImmediate(() => {
+      runDeploymentScript()
+        // eslint-disable-next-line no-console
+        .then(() => console.log("Deployment finished"))
+        .catch((e) => console.error("Deployment error:", e));
+    });
 
     return res.status(200).json({ message: "Deployment started" });
   } catch (err) {
