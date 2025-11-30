929d83efeadfe3f60c3c02463f37cbab93c311e8
MerlindCPohl <merlind.pohl@student.htw-berlin.de>
2025-11-30 23:20:54 +0100

    deletes the module declaration from mui.d.ts and keeps the theme palette in theme.ts

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,24 +1,5 @@
 import { createTheme } from "@mui/material/styles";
 
-declare module "@mui/material/styles" {
-  interface Palette {
-    quiz: {
-      buttonColor: string;
-      buttonChecked: string;
-      cardBackground: string;
-      textColor: string;
-    };
-  }
-  interface PaletteOptions {
-    quiz?: {
-      buttonColor?: string;
-      buttonChecked?: string;
-      cardBackground?: string;
-      textColor?: string;
-    };
-  }
-}
-
 const theme = createTheme({
   palette: {
     mode: "light",
