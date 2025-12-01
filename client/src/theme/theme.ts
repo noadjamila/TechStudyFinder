d69b7f08c25a63f58bb8ff83d52e85b39a5761b9
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-12-01 22:39:12 +0100

    Add colours and necessary interfaces

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,13 +1,60 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface Theme {
+    custom: {
+      primaryButton: string;
+      secondaryButton: string;
+      tertiaryButton: string;
+      secondaryBorder: string;
+      tertiaryBorder: string;
+    };
+  }
+  interface ThemeOptions {
+    custom?: {
+      primaryButton?: string;
+      secondaryButton?: string;
+      tertiaryButton?: string;
+      secondaryBorder?: string;
+      tertiaryBorder?: string;
+    };
+  }
+  interface Palette {
+    quiz: {
+      buttonChecked: string;
+      cardBackground: string;
+      progressBg: string;
+      progressFill: string;
+    };
+  }
+  interface PaletteOptions {
+    quiz?: {
+      buttonChecked?: string;
+      cardBackground?: string;
+      progressBg?: string;
+      progressFill?: string;
+    };
+  }
+}
+
 const theme = createTheme({
+  custom: {
+    primaryButton: "#AFCEFF",
+    secondaryButton: "#FFFFFF",
+    tertiaryButton: "#FFFFFF",
+    secondaryBorder: "#AFCEFF",
+    tertiaryBorder: "#D9D9D9",
+  },
   palette: {
     mode: "light",
+    text: {
+      primary: "#3F3E42",
+    },
     quiz: {
-      buttonColor: "#AFCEFF",
       buttonChecked: "#AFCEFF",
       cardBackground: "#E2FBBE",
-      textColor: "#3F3E42",
+      progressBg: "#FFD7F5",
+      progressFill: "#FFA5E9",
     },
   },
   typography: {
