725d6a1a9986363c1b07238dd8e6c326b9340de3
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2025-12-02 21:49:55 +0100

    Fix level 1 progress bar

--- client/src/pages/QuizPage_L1.tsx
+++ client/src/pages/QuizPage_L1.tsx
@@ -6,7 +6,6 @@ import QuizMascot from "../assets/Männchen_Home_Level1.png";
 
 /** Callback function executed upon successful completion of the level.
  * It receives an array of filtered IDs from the backend. */
-
 export interface QuizPageL1Props {
   onNextLevel?: (_ids: number[]) => void;
 }
@@ -15,7 +14,6 @@ export interface QuizPageL1Props {
  * Constants defining the options for the Level 1 question.
  * The value field is used in the API payload.
  */
-
 const L1_OPTIONS = [
   { label: "ein Studium beginnen?", value: "grundständig" },
   { label: "dein Studium fortsetzen?", value: "weiterführend" },
@@ -60,7 +58,7 @@ export default function QuizPage_L1({ onNextLevel }: QuizPageL1Props) {
   };
 
   return (
-    <QuizLayout currentIndex={1} questionsTotal={1}>
+    <QuizLayout currentIndex={selected ? 1 : 0} questionsTotal={1}>
       <QuizCardBase
         question="Möchtest du..."
         options={L1_OPTIONS}
