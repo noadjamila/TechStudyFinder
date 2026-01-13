66158a5c57944f2947d29fd2ebfaa9838c177940
MerlindCPohl <merlind.pohl@student.htw-berlin.de>
2026-01-13 18:13:53 +0100

    changes the READ ME, cleans up docker and init file for databases

--- server/db/schema/users.sql
+++ server/db/schema/users.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Create index on username for faster lookups
