54787b0473e5d21be59097a04517977836b671af
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2026-01-26 20:10:18 +0100

    Explicitly load .env.test

--- server/jest.setup.ts
+++ server/jest.setup.ts
@@ -1,3 +1,10 @@
+import path from "path";
+import dotenv from "dotenv";
+
 process.env.GITHUB_WEBHOOK_SECRET = "test-secret-for-env";
 process.env.SESSION_SECRET = "test-session-secret";
 process.env.NODE_ENV = "test";
+
+const dotenvPath =
+  process.env.DOTENV_CONFIG_PATH || path.join(__dirname, ".env.test");
+dotenv.config({ path: dotenvPath, override: true });
